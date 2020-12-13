22481fd9af814f5b54d9b1c0a3ab2b4755adaea5
Bapic <43567337+Bapic@users.noreply.github.com>
2020-12-13 21:20:52 +0530

    updated code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Good Morning Everyone!!! App Service is awesome!');
+    res.send('Good Morning Everyone!!! App Service is awesome');
 });
 
 app.listen(80, () => { console.log('Listening on port 8080') })
